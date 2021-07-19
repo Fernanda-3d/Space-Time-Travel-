4af9f749fc3574ae692fb55863877d4458adb858
Fernanda-3d <81561132+Fernanda-3d@users.noreply.github.com>
2021-07-19 14:08:29 +0100

    19-07-2021 - 1

---
+++ RocketGame/Assets/Scripts/LightsOnOff.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightsOnOff : MonoBehaviour
+{ /*
+
+    public Material gray_mat;
+    public Material light_mat;
+    private bool changeMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        changeMaterial = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            changeColor();
+        }
+    }
+
+    private void changeColor()
+    {
+        if(changeMaterial == private void OnRenderImage(RenderTexture src, RenderTexture dest) {
+            {
+                gray_mat.
+            }
+        }
+    } */
+} 
